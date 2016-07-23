21dbb37a527cf177e7c6d5f254d9b9a9cb81cb55
cludden <chris.ludden@gmail.com>
2016-07-23 17:02:02 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,264 @@
+# ajv-moment
+an [ajv](https://github.com/epoberezkin/ajv) plugin using [moment](https://github.com/moment/moment/) for robust date validation in your json-schemas.
+
+
+
+## Installing
+```bash
+npm install --save ajv moment ajv-moment
+```
+
+
+
+## Getting Started
+A few things to keep in mind, this plugin relies on [moment.js](https://github.com/moment/moment/) for date validation. In order to take advantage of moment.js with ajv, two things need to happen. First, the ajv instance responsible for compiling/validating schemas must be instantiated with the `passContext` flag enabled. Second, validation calls must be bound to an object that contains a reference to moment.js. This is for two reasons:
+
+1. It allows us to avoid requiring moment.js to be present in the global namespace.
+2. It allows you, the user, to include additional context data for use in other ajv plugins.
+
+```javascript
+const Ajv = require('ajv');
+const AjvMoment = require('ajv-moment');
+const moment = require('moment');
+
+// define an ajv instance with the 'passContext' flag enabled
+const ajv = new Ajv({ passContext: true });
+
+// install the plugin. by default, this plugin utilizes the "moment" custom keyword
+const ajvMoment = new AjvMoment();
+ajvMoment.plugin(ajv);
+
+// define your schemas using the "moment" keyword
+// here we define a schema for an object with two keys (assigned & due)
+// each of which must be a valid date string in ISO 8601 format.
+// in addition, we specify that the due key must be greater than or
+// equal to the assigned date plus 3 days.
+const schema = {
+    type: 'object',
+    properties: {
+        assigned: {
+            type: 'string',
+            moment: true
+        },
+        due: {
+            type: 'string',
+            moment: {
+                validate: [{
+                    test: 'isSameOrAfter',
+                    value: {
+                        $data: '1/assigned',
+                        manipulate: [{
+                            add: [3, 'days']
+                        }]
+                    }
+                }]
+            }
+        }
+    },
+    required: [
+        'assigned', 'due'
+    ]
+}
+
+// compile your schema using ajv and call it bound to an object that contains
+// a reference to moment.
+const ctx = { moment };
+const validate = ajv.compile(schema);
+
+
+validate.call(ctx, {
+    assigned: new Date().toISOString(),
+    due: moment().add(24, 'hours').toISOString()
+});
+// false
+
+validate.call(ctx, {
+    assigned: new Date().toISOString(),
+    due: moment().add(1, 'weeks').toISOString()
+});
+// true
+```
+
+
+
+## Schemas
+The custom keyword schema definitions can take on many forms. The simplest being the following:
+
+```json
+{
+    "type": "string",
+    "description": "a valid date string in ISO 8601 format",
+    "moment": true
+}
+```
+
+This schema will simply enforce that the provided value is a valid date string in [ISO 8601](https://en.wikipedia.org/wiki/ISO_8601) format.
+
+To specify a custom format, a `format` option can be defined in the schema definition. When a custom format is specified, moment's `strict` parsing option will be enforced.
+
+```json
+{
+    "type": "string",
+    "description": "a valid date string in MM-DD-YYYY format",
+    "moment": {
+        "format": ["MM-DD-YYYY"]
+    }
+}
+```
+
+Aside from date string formats, this plugin can also perform additional validations using `moment`. The validations are defined in the `validate` subschema keyword and can be used to enforce relative constraints on date strings. See below for some examples.
+
+```json
+[
+    {
+        "type": "string",
+        "description": "a valid date string prior to January 1st, 2000 UTC",
+        "moment": {
+            "validate": [
+                {
+                    "test": "isBefore",
+                    "value": "2000-01-01T00:00:00.000Z"
+                }
+            ]
+        }
+    },
+    {
+        "type": "object",
+        "properties": {
+            "assigned": {
+                "type": "string",
+                "description": "a valid date string in ISO 8601 format",
+                "moment": true
+            },
+            "due": {
+                "type": "string",
+                "description": "a valid date string in ISO 8601 format that is greater than or equal to the assigned date plus 3 days & 30 minutes",
+                "moment": {
+                    "validate": [
+                        {
+                            "test": "isSameOrAfter",
+                            "value": {
+                                "$data": "1/assigned",
+                                "manipulate": [
+                                    {
+                                        "add": [3, "days"]
+                                    },
+                                    {
+                                        "add": [30, "minutes"]
+                                    }
+                                ]
+                            }
+                        }
+                    ]
+                }
+            }
+        },
+        "required": [
+            "assigned",
+            "due"
+        ]
+    },
+    {
+        "type": "object",
+        "properties": {
+            "first": {
+                "type": "string",
+                "description": "a valid date string in ISO 8601 format",
+                "moment": true
+            },
+            "second": {
+                "type": "string",
+                "description": "a valid date string in ISO 8601 format that is greater than the 'first' attribute by at least 30 minutes",
+                "moment": {
+                    "validate": [
+                        {
+                            "test": "isSameOrAfter",
+                            "value": {
+                                "$data": "1/first",
+                                "manipulate": [
+                                    {
+                                        "add": [30, "minutes"]
+                                    }
+                                ]
+                            }
+                        }
+                    ]
+                }
+            },
+            "breakpoint": {
+                "type": "string",
+                "description": "a valid date string in ISO 8601 format that is both between 'first' and 'second' and also prior to 'second' by at least 15 seconds",
+                "moment": {
+                    "validate": [
+                        {
+                            "test": "isBetween",
+                            "value": [
+                                {
+                                    "$data": "1/first"
+                                },
+                                {
+                                    "$data": "1/second"
+                                }
+                            ]
+                        },
+                        {
+                            "test": "isSameOrBefore",
+                            "value": {
+                                "$data": "1/second",
+                                "manipulate": [
+                                    {
+                                        "subtract": [15, "seconds"]
+                                    }
+                                ]
+                            }
+                        }
+                    ]
+                }
+            },
+            "third": {
+                "type": "string",
+                "description": "a valid date string prior in ISO 8601 format that after 'second'",
+                "moment": {
+                    "validate": [
+                        {
+                            "test": "isAfter",
+                            "value": {
+                                "$data": "1/second"
+                            }
+                        }
+                    ]
+                }
+            }
+        },
+        "required": [
+            "first",
+            "second",
+            "breakpoint",
+            "third"
+        ]
+    }
+]
+```
+
+
+
+## Testing
+run all tests  
+```javascript
+npm test
+```
+
+
+
+## Contributing
+1. [Fork it](https://github.com/cludden/ajv-moment/fork)
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create new Pull Request
+
+
+
+## License
+Copyright (c) 2016 Chris Ludden.
+Licensed under the [MIT license](LICENSE.md).
---
+++ test/index.test.js
@@ -0,0 +1,345 @@
+'use strict';
+
+
+const Ajv = require('ajv');
+const AjvMoment = require('../lib');
+const expect = require('chai').expect;
+const moment = require('moment');
+const _ = require('lodash');
+
+describe('ajv-dates', function() {
+    const now = moment();
+    const ajvMoment = new AjvMoment();
+    const ajv = new Ajv({ passContext: true });
+
+    before(function() {
+        ajvMoment.plugin(ajv);
+    });
+
+    const schemas = [{
+        name: 'basic',
+        schema: {
+            type: 'string',
+            moment: true
+        },
+        tests: [{
+            data: new Date().toISOString(),
+            result: true
+        },{
+            data: 'hello',
+            result: false
+        }]
+    },{
+        name: 'custom-format',
+        schema: {
+            type: 'string',
+            moment: {
+                format: ['MM-DD-YYYY']
+            }
+        },
+        tests: [{
+            data: '12-01-2010',
+            result: true
+        },{
+            data: '12/1/2010',
+            result: false
+        }]
+    },{
+        name: 'isBefore',
+        schema: {
+            type: 'object',
+            properties: {
+                start: {
+                    type: 'string',
+                    moment: {
+                        validate: [{
+                            test: 'isBefore',
+                            value: {
+                                $data: '1/finish'
+                            }
+                        }]
+                    }
+                },
+                finish: {
+                    type: 'string',
+                    moment: true
+                }
+            }
+        },
+        tests: [{
+            data: {
+                start: moment().subtract(1, 'milliseconds').toISOString(),
+                finish: new Date().toISOString()
+            },
+            result: true
+        },{
+            data: {
+                start: moment('10/31/2010', 'MM/DD/YYYY').add(1, 'milliseconds').toISOString(),
+                finish: moment('10/31/2010', 'MM/DD/YYYY').subtract(1, 'milliseconds').toISOString()
+            },
+            result: false
+        }]
+    },{
+        name: 'isAfter',
+        schema: {
+            type: 'object',
+            properties: {
+                created: {
+                    type: 'string',
+                    moment: true
+                },
+                due: {
+                    type: 'string',
+                    moment: {
+                        validate: [{
+                            test: 'isAfter',
+                            value: {
+                                $data: '1/created',
+                                manipulate: [
+                                    { add: [1, 'days'] },
+                                    { set: { hour: 17, minute: 30 } }
+                                ]
+                            }
+                        }]
+                    }
+                }
+            }
+        },
+        tests: [{
+            data: {
+                created: moment().subtract(1, 'weeks').toISOString(),
+                due:  new Date().toISOString()
+            },
+            result: true
+        },{
+            data: {
+                created: moment().subtract(30, 'minutes').toISOString(),
+                due: new Date().toISOString()
+            },
+            result: false
+        }]
+
+    },{
+        name: 'isSameOrAfter',
+        schema: {
+            type: 'object',
+            properties: {
+                nested: {
+                    type: 'object',
+                    properties: {
+                        created: {
+                            type: 'string',
+                            moment: true
+                        }
+                    }
+                },
+                due: {
+                    type: 'string',
+                    moment: {
+                        validate: [{
+                            test: 'isSameOrAfter',
+                            value: {
+                                $data: '1/nested/created',
+                                manipulate: [
+                                    { add: [1, 'weeks'] },
+                                    { endOf: ['day'] }
+                                ]
+                            }
+                        }]
+                    }
+                }
+            }
+        },
+        tests: [{
+            data: {
+                nested: {
+                    created: new Date().toISOString()
+                },
+                due: moment().add(1, 'weeks').endOf('day').toISOString()
+            },
+            result: true
+        },{
+            data: {
+                nested: {
+                    created: new Date().toISOString()
+                },
+                due: moment().add(2, 'weeks').toISOString()
+            },
+            result: true
+        },{
+            data: {
+                nested: {
+                    created: new Date().toISOString()
+                },
+                due: moment().add(1, 'days').endOf('day').subtract(30, 'minutes').toISOString()
+            },
+            result: false
+        }]
+    },{
+        name: 'isBetween',
+        schema: {
+            type: 'object',
+            properties: {
+                start: {
+                    type: 'string',
+                    moment: true
+                },
+                middle: {
+                    type: 'string',
+                    moment: {
+                        validate: [{
+                            test: 'isBetween',
+                            value: [{
+                                $data: '1/start',
+                                manipulate: [
+                                    { add: [1, 'hours'] }
+                                ]
+                            },{
+                                $data: '1/finish',
+                                manipulate: [
+                                    { subtract: [1, 'hours'] }
+                                ]
+                            }]
+                        }]
+                    }
+                },
+                finish: {
+                    type: 'string',
+                    moment: {
+                        validate: [{
+                            test: 'isAfter',
+                            value: {
+                                $data: '1/start',
+                                manipulate: [
+                                    { add: [36, 'hours'] }
+                                ]
+                            }
+                        }]
+                    }
+                }
+            }
+        },
+        tests: [{
+            data: {
+                start: new Date().toISOString(),
+                middle: moment().add(2, 'hours').toISOString(),
+                finish: moment().add(2, 'days').toISOString()
+            },
+            result: true
+        },{
+            data: {
+                start: new Date().toISOString(),
+                middle: moment().add(2, 'days').subtract(1, 'hours').subtract(1, 'seconds').toISOString(),
+                finish: moment().add(2, 'days').toISOString()
+            },
+            result: true
+        },{
+            data: {
+                start: new Date().toISOString(),
+                middle: moment().add(1, 'hours').subtract(1, 'milliseconds').toISOString(),
+                finish: moment().add(2, 'days').toISOString()
+            },
+            result: false
+        },{
+            data: {
+                start: new Date().toISOString(),
+                middle: moment().add(47, 'hours').add(1, 'milliseconds').toISOString(),
+                finish: moment().add(2, 'days').toISOString()
+            },
+            result: false
+        }]
+    },{
+        name: 'complex',
+        schema: {
+            type: 'object',
+            properties: {
+                first: {
+                    type: 'string',
+                    moment: true
+                },
+                breakpoint: {
+                    type: 'string',
+                    moment: {
+                        validate: [{
+                            test: 'isBetween',
+                            value: [{
+                                $data: '1/first'
+                            },{
+                                $data: '1/second'
+                            }]
+                        },{
+                            test: 'isSameOrBefore',
+                            value: {
+                                $data: '1/second',
+                                manipulate: [
+                                    { subtract: [15, 'seconds' ] }
+                                ]
+                            }
+                        }]
+                    }
+                },
+                second: {
+                    type: 'string',
+                    moment: {
+                        validate: [{
+                            test: 'isSameOrAfter',
+                            value: {
+                                $data: '1/first',
+                                manipulate: [
+                                    { add: [30, 'minutes'] }
+                                ]
+                            }
+                        }]
+                    }
+                },
+                third: {
+                    type: 'string',
+                    moment: {
+                        validate: [{
+                            test: 'isAfter',
+                            value: {
+                                $data: '1/second'
+                            }
+                        }]
+                    }
+                }
+            },
+            required: [
+                'first', 'second', 'third', 'breakpoint'
+            ]
+        },
+        tests: [{
+            data: {
+                first: now.toISOString(),
+                breakpoint: now.clone().add(29, 'minutes').add(45, 'seconds').toISOString(),
+                second: now.clone().add(30, 'minutes').toISOString(),
+                third: now.clone().add(30, 'minutes').add(1, 'milliseconds').toISOString()
+            },
+            result: true
+        },{
+            data: {
+                first: now.toISOString(),
+                breakpoint: now.clone().add(29, 'minutes').add(45, 'seconds').subtract(1, 'milliseconds').toISOString(),
+                second: now.clone().add(30, 'minutes').subtract(1, 'milliseconds').toISOString(),
+                third: now.clone().add(30, 'minutes').add(1, 'milliseconds').toISOString()
+            },
+            result: false
+        }]
+    }];
+
+    schemas.forEach(function(schema) {
+        schema.tests.forEach(function(test, ii) {
+            const outcome = test.result === true ? 'pass' : 'fail';
+            it(`should ${outcome} validation (${schema.name}, test ${ii})`, function() {
+                const momentify = { moment };
+                const validate = ajv.compile(schema.schema);
+                const e = _.attempt(function() {
+                    expect(validate.call(momentify, test.data)).to.equal(test.result);
+                });
+                if (_.isError(e)) {
+                    throw e;
+                }
+            });
+        });
+    });
+});
